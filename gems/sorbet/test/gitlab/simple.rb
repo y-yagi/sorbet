ff93f25b837d92a0f804666ad2c47ed896b78f3e
Neil Parikh <neil@stripe.com>
2019-04-18 18:19:52 -0700

    test

--- gems/sorbet/test/gitlab/simple.rb
+++ gems/sorbet/test/gitlab/simple.rb
@@ -36,6 +36,8 @@ class Sorbet::Private::Gitlab::Test::Simple < MiniTest::Spec
         # IO.popen(olddir + '/../../bin/srb-rbi') do
         # end
         system('bundle install')
+        system('ls')
+        system('cat Gemfile')
         system('printf "gem \'sorbet\'" >> Gemfile')
         # system('gem install ' + olddir + '/../../../gems/sorbet-static/sorbet-static-0.0.0.gem')
         ENV['SRB_YES'] = '1'
