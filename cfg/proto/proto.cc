70c2f5362afc82b8321c9e64e0e5311f06f506aa
Dave Vasilevsky <vasi@stripe.com>
2019-04-29 21:22:57 -0400

    variables

--- cfg/proto/proto.cc
+++ cfg/proto/proto.cc
@@ -7,9 +7,27 @@ using namespace std;
 
 namespace sorbet::cfg {
 
-com::stripe::rubytyper::BasicBlock Proto::toProto(const core::GlobalState &gs, const cfg::BasicBlock &bb) {
-    com::stripe::rubytyper::BasicBlock proto;
+com::stripe::rubytyper::TypedVariable Proto::toProto(const core::GlobalState &gs, const cfg::VariableUseSite &vus) {
+    com::stripe::rubytyper::TypedVariable proto;
+    // TODO: Is `shortName` really what we want?
+    auto const &name = vus.variable._name.data(gs);
+    proto.set_variable(std::string(name->shortName(gs)));
+    return proto;
+}
+
+
+com::stripe::rubytyper::Binding Proto::toProto(const core::GlobalState &gs, const cfg::Binding &bnd) {
+    com::stripe::rubytyper::Binding proto;
+    *proto.mutable_bind() = toProto(gs, bnd.bind);
+    return proto;
+}
+
+com::stripe::rubytyper::Block Proto::toProto(const core::GlobalState &gs, const cfg::BasicBlock &bb) {
+    com::stripe::rubytyper::Block proto;
     proto.set_id(bb.id);
+    for (auto const &bnd: bb.exprs) {
+        *proto.add_bindings() = toProto(gs, bnd);
+    }
     return proto;
 }
 
@@ -17,7 +35,7 @@ com::stripe::rubytyper::CFG Proto::toProto(const core::GlobalState &gs, const cf
     com::stripe::rubytyper::CFG proto;
     *proto.mutable_symbol() = core::Proto::toProto(gs, cfg.symbol);
     for (auto const &block: cfg.basicBlocks) {
-        *proto.add_basic_blocks() = toProto(gs, *block);
+        *proto.add_blocks() = toProto(gs, *block);
     }
     return proto;
 }
--- cfg/proto/proto.h
+++ cfg/proto/proto.h
@@ -10,7 +10,9 @@ namespace sorbet::cfg {
     public:
         Proto() = delete;
 
-        static com::stripe::rubytyper::BasicBlock toProto(const core::GlobalState &gs, const cfg::BasicBlock &bb);
+        static com::stripe::rubytyper::TypedVariable toProto(const core::GlobalState &gs, const cfg::VariableUseSite &vus);
+        static com::stripe::rubytyper::Binding toProto(const core::GlobalState &gs, const cfg::Binding &bnd);
+        static com::stripe::rubytyper::Block toProto(const core::GlobalState &gs, const cfg::BasicBlock &bb);
         static com::stripe::rubytyper::CFG toProto(const core::GlobalState &gs, const cfg::CFG &cfg);
     };
 } // namespace sorbet::core
